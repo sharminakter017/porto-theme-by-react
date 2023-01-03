fc5f0034570532ea01ddd49348f143a785aeb614
Sharminakter017 <mssharminakter2000@gmail.com>
2023-01-03 18:11:05 +0600

    all files upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,299 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+ <div className="topheader">
+     <div className="container">
+      <div className="row">
+        <div className="col-md-8"></div>
+        <div className="col-md-4">
+          <div className="topbar">
+            <ul>
+              <li><a href="#"><i className='bx bx-chevron-right'></i>About Us</a></li>
+              <li><a href="#"><i className='bx bx-chevron-right'></i>Contact Us</a></li>
+              <li><a href="#"><i className='bx bxs-phone'></i>(123) 456-7890</a></li>
+              <li><a href="#"><i className='bx bx-search'></i></a></li>
+              <li><a href="#"><i className='bx bx-shopping-bag'></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+    <div className="second-header">
+  <div className="container">
+    <div className="row align-items-center">
+      <div className="col-md-4">
+        <div className="logo">
+          <img src="https://www.portotheme.com/wordpress/porto/wp-content/themes/porto/images/logo/logo-default-slim-2.png" alt="" />
+        </div>
+      </div>
+      <div className="col-md-8">
+        <div className="menu">
+          <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">ELEMENTS</a></li>
+            <li><a href="#">FEATURES</a></li>
+            <li><a href="#">PAGES</a></li>
+            <li><a href="#">BLOG</a></li>
+            <li><a href="#">PORTFOLIO</a></li>
+            <li><a href="#">BUY PORTO!</a></li>
+            <li><a href="#"><i className='bx bxl-facebook-circle' ></i></a></li>
+            <li><a href="#"><i className='bx bxl-twitter' ></i></a></li>
+            <li><a href="#"><i className='bx bxl-linkedin' ></i></a></li>
+          </ul>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+ </div>
+
+  <div className="main-body">
+    <div className="background">
+   <h2>DO YOU NEED A NEW</h2> 
+   <h1>WEB DESIGN?</h1>
+   <p>Check out our options and features</p>
+    </div>
+  </div>
+
+  <div className="nil">
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-md-8">
+          <div className="nil-left">
+            <h2>The fastest way to grow your business with the leader in <span>Technology</span></h2>
+            <p>Check out our options and features included.</p>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="baton">
+          <ul className="align-items-center">
+            <li>Get Started Now</li>
+            <li>Or lern more</li>
+          </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div className="heading">
+    <h2>Porto is extremely beautiful and fully responsive.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum, nulla vel pellentesque consequat, ante nulla hendrerit arcu, ac tincidunt mauris lacus sed leo.</p>
+  </div>
+
+
+  <div className="sun">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2"></div>
+     
+        <div className="col-md-2">
+          
+          <div className="cir cirr">
+            <img className="img1" src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/home-concept-item-1.png" alt="" />
+            <h3>Strategy</h3>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="cir">
+            <img  src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/home-concept-item-2.png" alt="" />
+            <h3>Planning</h3>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="cir cirrr">
+            <img src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/home-concept-item-3.png" alt="" />
+            <h3>Build</h3>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="cir1">
+            <img src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/project-home-1.jpg" alt="" />
+            <h3>Our Work</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<div className="allicon">
+<div className="container">
+    <h2>Our <b>Features</b> </h2>
+    <div className="row align-items-center">
+      <div className="col-md-4">
+        <div className="fea align-items-center">
+          <h3> <i class='bx bx-cog colo'></i> Customer Support</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+        <div className="fea">
+          <h3> <i class='bx bxs-file-doc colo' ></i> HTML5 / CSS3 / JS</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+        <div className="fea">
+          <h3> <i class='bx bxl-google-plus colo'></i> 500+ Google Fonts</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+        <div className="fea">
+          <h3> <i class='bx bx-pencil colo' ></i> Colors</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+      </div>
+      <div className="col-md-4">
+      <div className="fea align-items-center">
+          <h3> <i class='bx bx-layer colo' ></i> Sliders</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+        <div className="fea">
+          <h3> <i class='bx bxs-user-voice colo' ></i> Icons</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+        <div className="fea">
+          <h3> <i class='bx bx-menu colo' ></i> Buttons</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+        <div className="fea">
+          <h3> <i class='bx bx-desktop colo' ></i> Lightbox</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blandit massa</p>
+        </div>
+      </div>
+      <div className="col-md-4">
+        <div className="diamonds">
+          <div className="card">
+            <div className="card-header"><h3><i class='bx bx-diamond colo' ></i>Creative Websites</h3></div>
+            <div className="card-body">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus blanorem ipsum dolor sit amet, consecte.<br></br> <br></br>
+
+               Adipiscing elit phasellus blanit ma…</p>
+               <a href="#">read more <i class='bx bx-chevron-right'></i></a>
+            </div>
+            <div className="card-footer"> <p><i class='bx bx-envelope'></i>Contact Forms</p></div>
+            <div className="card-footer"> <p><i class='bx bxs-grid-alt' ></i>Portfolio Pages</p></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div className="heading">
+    <h2>We're not the only ones happy excited about Porto WordPress Theme...</h2>
+    <p>20,000 CUSTOMERS IN 100 COUNTRIES USE PORTO TEMPLATE. MEET OUR CUSTOMERS.</p>
+  </div>
+
+  <div className="container">
+    <div className="row">
+      <div className="col-md-3"></div>
+      <div className="col-md-1 logos">
+        <img className="loges" src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/logo-1.png" alt="" />
+      </div>
+      <div className="col-md-1 logos">
+        <img className="loges" src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/logo-2.png" alt="" />
+      </div>
+      <div className="col-md-1 logos">
+        <img className="loges" src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/logo-3.png" alt="" />
+      </div>
+      <div className="col-md-1 logos">
+        <img className="loges" src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/logo-4.png" alt="" />
+      </div>
+      <div className="col-md-1 logos">
+        <img className="loges" src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/logo-5.png" alt="" />
+      </div>
+      <div className="col-md-1 logos">
+        <img className="loges" src="https://www.portotheme.com/wordpress/porto/wp-content/uploads/images/logo-6.png" alt="" />
+      </div>
+      <div className="col-md-3"></div>
+    </div>
+  </div>
+
+  <div className="aimg">
+    <img className="ss" src="https://www.portotheme.com/wordpress/porto/classic-original/wp-content/themes/porto/images/map.png" alt="" />
+  </div>
+
+  <div className="then">
+    <img className="oo" src="https://i.postimg.cc/TwC7ZnRQ/sx.png" alt="" />
+  </div>
+
+ 
+  
+  <div className="footer">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="newslater">
+            <h4>NEWSLETTER</h4>
+            <p>Keep up on our always evolving product features and technology. Enter your e-mail and subscribe to our newsletter.</p>
+            <input name="email" className="form-control emails" placeholder="email address" type="email" />
+          </div>
+        </div>
+        <div className="col-md-3">
+         <div className="newslater">
+         <h4>LATEST TWEETS</h4>
+         <i class='bx bxl-twitter twet'></i>
+          <p>Glad to notify that we partnered up with Amasty, a Premier Magento Extension Builder with offices in Belarus, Slova… https://t.co/zqUEV0tyWH
+          01:26 AM Oct 05th</p> <br></br> 
+          <i class='bx bxl-twitter twet'></i>
+          <p>Annual big sale is now live! Please check our mega march campaign for Porto wordpress theme! https://t.co/hantQ1Ipde
+          09:38 AM Mar 25th</p>
+         </div>
+        </div>
+        <div className="col-md-3">
+        <div className="newslater">
+            <h4>CONTACT US</h4>
+           <ul>
+            <li> <i class='bx bx-radio-circle-marked '></i> Address: 1234 Street Name, City </li>
+            <li> <i class='bx bxl-whatsapp ' ></i>Phone: (123) 456-7890</li>
+            <li> <i class='bx bx-envelope' ></i> Email: mail@example.com</li>
+            <li><i class='bx bxs-color ' ></i> Working Days/Hours: Mon - Sun.</li>
+           </ul>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="newslater">
+            <h4>FOLLOW US</h4>
+            <div className="linkicons">
+              <ul>
+                <li><i class='bx bxl-facebook'></i></li>
+                <li><i class='bx bxl-twitter' ></i></li>
+                <li><i class='bx bxl-linkedin'></i></li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+ </div>
+ <div className="footer-bottom">
+  <div className="container">
+    <div className="row align-items-center">
+      <div className="col-md-9">
+        <div className="left-b align-items-center">
+          <img src="https://www.portotheme.com/wordpress/porto/classic-original/wp-content/themes/porto/images/logo/logo_footer.png" alt="" />
+          <p> © copyright 2022. All Rights Reserved.</p>
+        </div>
+      </div>
+      <div className="col-md-3">
+        <div className="lis">
+          <ul>
+            <li><i class='bx bx-chevron-right'></i>sitemap</li>
+            <li><i class='bx bx-chevron-right'></i>contact</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+ </div>
+ </div>
+
+
+ 
+
+
+  )
 }
 
 export default App;
